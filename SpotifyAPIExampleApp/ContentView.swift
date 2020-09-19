0445bbb689c88d8535d9700c884ff6d5d2851409
Peter-Schorn <petervschorn@gmail.com>
2020-09-19 02:03:57 -0500

    Initial Commit

---
+++ SpotifyAPIExampleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpotifyAPIExampleApp
+//
+//  Created by Peter Schorn on 9/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SpotifyAPIExampleApp/SpotifyAPIExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyAPIExampleAppApp.swift
+//  SpotifyAPIExampleApp
+//
+//  Created by Peter Schorn on 9/19/20.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyAPIExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
