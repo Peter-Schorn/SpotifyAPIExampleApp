f981da8dcb33e6c7c644d5cacd8a1bea9636d33c
Peter-Schorn <petervschorn@gmail.com>
2021-06-03 14:34:02 -0500

    Renamed "miscellaneousUtilities.swift" to "MiscellaneousUtilities.swift".  Fixed some documentation comment issues. Added the accessibility identifier to the albums grid view.

---
+++ SpotifyAPIExampleApp/Project Utilities/MiscellaneousUtilities.swift
@@ -0,0 +1,22 @@
+import Foundation
+import SwiftUI
+import SpotifyWebAPI
+
+extension View {
+    
+    /// Type erases self to `AnyView`. Equivalent to `AnyView(self)`.
+    func eraseToAnyView() -> AnyView {
+        return AnyView(self)
+    }
+
+}
+
+extension ProcessInfo {
+    
+    /// Whether or not this process is running within the context of
+    /// a SwiftUI preview.
+    var isPreviewing: Bool {
+        return self.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
+    }
+
+}
