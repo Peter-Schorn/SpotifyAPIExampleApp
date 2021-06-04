24f6407b14ae5c881e27cd2a064a3dbb714691a6
Peter-Schorn <petervschorn@gmail.com>
2021-06-04 15:59:11 -0500

    Added DebugMenuView

---
+++ SpotifyAPIExampleApp/Views/DebugMenuView.swift
@@ -0,0 +1,49 @@
+import SwiftUI
+import Combine
+
+struct DebugMenuView: View {
+    
+    @EnvironmentObject var spotify: Spotify
+    
+    @State var cancellables: Set<AnyCancellable> = []
+
+    var body: some View {
+        List {
+            Button("Make Access Token Expired") {
+                self.spotify.api.authorizationManager.setExpirationDate(
+                    to: Date()
+                )
+            }
+            Button("Refresh Access Token") {
+                self.spotify.api.authorizationManager.refreshTokens(
+                    onlyIfExpired: false
+                )
+                .sink(receiveCompletion: { completion in
+                    print("refresh tokens completion: \(completion)")
+                    
+                })
+                .store(in: &self.cancellables)
+            }
+            Button("Print SpotifyAPI") {
+                print(
+                    """
+                    --- SpotifyAPI ---
+                    \(self.spotify.api)
+                    ------------------
+                    """
+                )
+            }
+            
+        }
+        .navigationBarTitle("Debug Menu")
+    }
+}
+
+struct DebugMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            DebugMenuView()
+        }
+        .environmentObject(Spotify())
+    }
+}
