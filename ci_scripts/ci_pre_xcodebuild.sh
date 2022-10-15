846a11bf35a4375768a6dd8303e352da2aa892bb
Peter Schorn <petervschorn@gmail.com>
2022-10-15 12:08:15 -0500

    Changed ci_pre_build.sh script.

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -3,7 +3,7 @@ cd SoftPlayer/Model
 
 source ~/.local/soft_player_credentials.sh
 
-sed -r -i '' "s~(let __clientId__ = )\".*\"~\1\"$CLIENT_ID\"~" 'Spotify.swift'
+sed -r -i '' "s~(let __clientId__ = )\".*\"~\1\"$SPOTIFY_SWIFT_TESTING_CLIENT_ID\"~" 'Spotify.swift'
 
-sed -r -i '' "s~(let __tokensURL__ = )\".*\"~\1\"$TOKENS_URL\"~" 'Spotify.swift'
-sed -r -i '' "s~(let __tokensRefreshURL__ = )\".*\"~\1\"$TOKENS_REFRESH_URL\"~" 'Spotify.swift'
+sed -r -i '' "s~(let __tokensURL__ = )\".*\"~\1\"$SPOTIFY_AUTHORIZATION_CODE_FLOW_TOKENS_URL\"~" 'Spotify.swift'
+sed -r -i '' "s~(let __tokensRefreshURL__ = )\".*\"~\1\"$SPOTIFY_AUTHORIZATION_CODE_FLOW_REFRESH_TOKENS_URL\"~" 'Spotify.swift'
